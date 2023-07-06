8c0a496bced5b008488db05e62596076774ff92c
Oscar Vallejo <oscarvallejoipuz@gmail.com>
2023-07-06 18:41:22 -0500

    CodigoExplicado

--- src/components/WeatherPanel.jsx
+++ src/components/WeatherPanel.jsx
@@ -1,26 +1,29 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Loader from "./Loader";
+import { useState, useEffect } from "react"; // Importar las funciones useState y useEffect de React
+import axios from "axios"; // Importar el módulo axios para realizar solicitudes HTTP
+import Loader from "./Loader"; // Importar el componente Loader desde un archivo local
 
-const API_KEY = "fd2fd95e5cb8abc849c754b063354de1";
-const API_URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather";
-
+const API_KEY = "fd2fd95e5cb8abc849c754b063354de1"; // Clave de la API de OpenWeatherMap
+const API_URL_WEATHER = "https://api.openweathermap.org/data/2.5/weather"; // URL base de la API de OpenWeatherMap
 
 const WeatherApp = () => {
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [weatherIcon, setWeatherIcon] = useState("");
-  const [temperatureCelsius, setTemperatureCelsius] = useState(null);
-  const [temperatureUnit, setTemperatureUnit] = useState("Celsius");
-  const [humidity, setHumidity] = useState(null);
-  const [windSpeed, setWindSpeed] = useState(null);
-  const [weatherDescription, setWeatherDescription] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
-  const [darkMode, setDarkMode] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [airPressure, setAirPressure] = useState(null);
+  // Definir el componente principal WeatherApp como una función de componente
+
+  // Estados del componente
+  const [city, setCity] = useState(""); // Ciudad actual
+  const [country, setCountry] = useState(""); // País actual
+  const [weatherIcon, setWeatherIcon] = useState(""); // Icono del clima actual
+  const [temperatureCelsius, setTemperatureCelsius] = useState(null); // Temperatura actual en Celsius
+  const [temperatureUnit, setTemperatureUnit] = useState("Celsius"); // Unidad de temperatura (Celsius, Fahrenheit, Kelvin)
+  const [humidity, setHumidity] = useState(null); // Humedad actual
+  const [windSpeed, setWindSpeed] = useState(null); // Velocidad del viento actual
+  const [weatherDescription, setWeatherDescription] = useState(""); // Descripción del clima actual
+  const [selectedCity, setSelectedCity] = useState(""); // Ciudad seleccionada por el usuario
+  const [darkMode, setDarkMode] = useState(false); // Modo oscuro
+  const [isLoading, setIsLoading] = useState(true); // Indicador de carga
+  const [airPressure, setAirPressure] = useState(null); // Presión atmosférica actual
 
   useEffect(() => {
+    // Efecto secundario para obtener los datos del clima de forma predeterminada o por geolocalización
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -39,11 +42,13 @@ const WeatherApp = () => {
   }, []);
 
   const fetchDefaultWeather = () => {
+    // Obtener los datos del clima por defecto (ciudad predeterminada)
     const defaultCity = "Bogota";
     fetchWeatherByCity(defaultCity);
   };
 
   const fetchWeatherByCoordinates = (latitude, longitude) => {
+    // Obtener los datos del clima por coordenadas geográficas
     axios
       .get(
         `${API_URL_WEATHER}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
@@ -68,6 +73,7 @@ const WeatherApp = () => {
   };
 
   const fetchWeatherByCity = (city) => {
+    // Obtener los datos del clima por nombre de ciudad
     axios
       .get(`${API_URL_WEATHER}?q=${city}&appid=${API_KEY}`)
       .then((response) => {
@@ -90,6 +96,7 @@ const WeatherApp = () => {
   };
 
   const toggleTemperatureUnit = () => {
+    // Cambiar la unidad de temperatura
     if (temperatureUnit === "Celsius") {
       setTemperatureUnit("Fahrenheit");
     } else if (temperatureUnit === "Fahrenheit") {
@@ -100,6 +107,7 @@ const WeatherApp = () => {
   };
 
   const renderTemperature = () => {
+    // Renderizar la temperatura con la unidad adecuada
     if (temperatureCelsius === null) {
       return null;
     }
@@ -116,6 +124,7 @@ const WeatherApp = () => {
   };
 
   const handleCitySubmit = (event) => {
+    // Manejar el envío del formulario
     event.preventDefault();
     if (selectedCity) {
       fetchWeatherByCity(selectedCity);
@@ -123,6 +132,7 @@ const WeatherApp = () => {
   };
 
   useEffect(() => {
+    // Efecto secundario para cambiar el modo oscuro o claro
     if (darkMode) {
       document.body.classList.add("dark-mode");
     } else {
@@ -132,6 +142,7 @@ const WeatherApp = () => {
 
   return (
     <section className={`app ${darkMode ? "dark-mode" : ""}`}>
+      {/* Botón para alternar entre el modo oscuro y claro */}
       <button
         className="dark-mode-toggle"
         onClick={() => setDarkMode(!darkMode)}
@@ -140,6 +151,7 @@ const WeatherApp = () => {
       </button>
       <div className="container">
         <h1>Weather App</h1>
+        {/* Formulario para ingresar la ciudad y obtener el clima */}
         <form onSubmit={handleCitySubmit}>
           <input
             type="text"
@@ -149,10 +161,12 @@ const WeatherApp = () => {
           />
           <button type="submit">Get Weather</button>
         </form>
+        {/* Mostrar el componente Loader mientras se obtienen los datos del clima */}
         {isLoading ? (
           <Loader />
         ) : (
           <>
+            {/* Mostrar los datos del clima */}
             <article className="card">
               {weatherIcon && (
                 <img
@@ -172,6 +186,7 @@ const WeatherApp = () => {
           </>
         )}
       </div>
+      {/* Botón para alternar entre las unidades de temperatura */}
       <button className="switch" onClick={toggleTemperatureUnit}>
         Switch Unit
       </button>
